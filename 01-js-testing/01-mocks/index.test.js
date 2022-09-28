e5a087d682e3a2345d9a5e70e0159b36f1e8bce9
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-09-28 11:22:23 -0300

    add four items file test

--- 01-js-testing/01-mocks/index.test.js
+++ 01-js-testing/01-mocks/index.test.js
@@ -4,9 +4,18 @@ const File = require('./src/file');
 const { error } = require('./src/constants');
 
 (async() => {
-  const filePath = './mocks/emptyFile-invalid.csv';
-  const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
-  const result = File.csvToJson(filePath);
+  {
+    const filePath = './mocks/emptyFile-invalid.csv';
+    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+    const result = File.csvToJson(filePath);
 
-  await rejects(result, rejection);
+    await rejects(result, rejection);
+  }
+  {
+    const filePath = './mocks/fourItems-invalid.csv';
+    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+    const result = File.csvToJson(filePath);
+
+    await rejects(result, rejection);
+  }
 })();

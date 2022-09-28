116964b9e77b69c5533abbb8566b8043ce395436
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-09-28 11:48:11 -0300

    add three items file test

--- 01-js-testing/01-mocks/index.test.js
+++ 01-js-testing/01-mocks/index.test.js
@@ -1,4 +1,4 @@
-const { rejects } = require('assert');
+const { rejects, deepStrictEqual } = require('assert');
 
 const File = require('./src/file');
 const { error } = require('./src/constants');
@@ -17,5 +17,33 @@ const { error } = require('./src/constants');
     const result = File.csvToJson(filePath);
 
     await rejects(result, rejection);
+  }
+  {
+    Date.prototype.getFullYear = () => 2020;
+
+    const filePath = './mocks/threeItems-valid.csv';
+    const expected = [
+      {
+        "id": 123,
+        "name": "Erick Wendel",
+        "profession": "JavaScript Instructor",
+        "birthYear": 1995
+      },
+      {
+        "id": 321,
+        "name": "John Doe",
+        "profession": "JavaScript Specialist",
+        "birthYear": 1940
+      },
+      {
+        "id": 231,
+        "name": "Jane Doe",
+        "profession": "Java Developer",
+        "birthYear": 1990
+      }
+    ]
+    const result = await File.csvToJson(filePath);
+
+    deepStrictEqual(JSON.stringify(result), JSON.stringify(expected));
   }
 })();

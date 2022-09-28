1583997c8b6741bc546238e06cff12f9f0ac2a8e
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-09-28 15:47:56 -0300

    add arguments test

--- 01-js-testing/03-spies/src/fibonacci.test.js
+++ 01-js-testing/03-spies/src/fibonacci.test.js
@@ -20,4 +20,29 @@ const Fibonacci = require("./fibonacci");
 
     assert.deepStrictEqual(spy.callCount, expectedCallCount);
   }
+  {
+    const EXECUTIONS = 5;
+
+    const fibonacci = new Fibonacci();
+    const spy = sinon.spy(fibonacci, fibonacci.execute.name);
+
+    const [...result] = fibonacci.execute(EXECUTIONS);
+    // [0] input = 5, current = 0, next = 1
+    // [1] input = 4, current = 1, next = 1
+    // [2] input = 3, current = 1, next = 2
+    // [3] input = 2, current = 2, next = 3
+    // [4] input = 1, current = 3, next = 5
+    // [5] input = 0 -> STOPS
+
+    const { args } = spy.getCall(2);
+    const expectedResult = [0, 1, 1, 2, 3];
+    const expectedParams = Object.values({
+      input: 3,
+      current: 1,
+      next: 2,
+    });
+
+    assert.deepStrictEqual(args, expectedParams);
+    assert.deepStrictEqual(result, expectedResult);
+  }
 })();

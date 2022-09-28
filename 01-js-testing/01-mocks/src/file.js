2642c1461e98a2b19f1b7d8e10c9c0edfd144ba4
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-09-28 11:36:30 -0300

    add parseCSVToJSON File class method

--- 01-js-testing/01-mocks/src/file.js
+++ 01-js-testing/01-mocks/src/file.js
@@ -1,47 +1,69 @@
-const { readFile } = require('fs/promises')
-const { error } = require('./constants')
+const { readFile } = require('fs/promises');
+const { error } = require('./constants');
 
 const DEFAULT_OPTIONS = {
   maxLines: 3,
   fields: ['id', 'name', 'profession', 'age'],
-}
+};
 
 class File {
   static async csvToJson(filePath) {
-    const content =  await File.getFileContent(filePath)
+    const content =  await File.getFileContent(filePath);
 
-    const validation = File.isValid(content)
+    const validation = File.isValid(content);
 
-    if (!validation.valid) throw new Error(validation.error)
+    if (!validation.valid) throw new Error(validation.error);
 
-    return content
-  }
+    const users = this.parseCSVToJSON(content);
+
+    return users;
+  };
 
   static async getFileContent(filePath) {
-    return (await readFile(filePath)).toString('utf8')
+    return (await readFile(filePath)).toString('utf8');
   }
 
   static isValid(csvString, options = DEFAULT_OPTIONS) {
-    const [header, ...lines] = csvString.split('\n')
+    const [header, ...lines] = csvString.split('\n');
 
-    const isHeaderValid = header === options.fields.join(',')
+    const isHeaderValid = header === options.fields.join(',');
     if (!isHeaderValid) {
       return {
         valid: false,
         error: error.FILE_FIELDS_ERROR_MESSAGE,
-      }
+      };
     }
 
-    const isLengthValid = lines.length > 0 && lines.length <= options.maxLines
+    const isLengthValid = lines.length > 0 && lines.length <= options.maxLines;
     if (!isLengthValid) {
       return {
         valid: false,
         error: error.FILE_LENGTH_ERROR_MESSAGE,
-      }
+      };
     }
 
-    return { valid: true }
+    return { valid: true };
   }
-}
 
-module.exports = File
+  static parseCSVToJSON(csvString) {
+    const lines = csvString.split('\n');
+
+    const firstLine = lines.shift();
+    const header = firstLine.split(',');
+
+    const users = lines.map(line => {
+      const items = line.split(',');
+      const user = {};
+
+      for (const index in items) {
+        user[header[index]] = items[index];
+      }
+
+      return user;
+    });
+
+    return users;
+  }
+};
+
+module.exports = File;

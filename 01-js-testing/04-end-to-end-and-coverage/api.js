2cba019c2a2e77201753ccc2c9917bdd1266f821
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-09-28 17:54:31 -0300

    add login route implementation

--- 01-js-testing/04-end-to-end-and-coverage/api.js
+++ 01-js-testing/04-end-to-end-and-coverage/api.js
@@ -1,6 +1,29 @@
 const http = require('http');
 
+const DEFAULT_USER = {
+  username: 'SomeUser',
+  password: 'somePassword',
+};
+
 const routes = {
+  '/login:post': async (request, response) => {
+    for await (const data of request) {
+      const user = JSON.parse(data);
+
+      if (
+        user.username !== DEFAULT_USER.username
+        || user.password !== DEFAULT_USER.password
+      ) {
+        response.writeHead(401);
+        response.write('Login failed!');
+        return response.end();
+      }
+
+      response.write('Login has succeeded!');
+      return response.end();
+    }
+  },
+
   '/contact:get': (_request, response) => {
     response.write('contact us page');
 

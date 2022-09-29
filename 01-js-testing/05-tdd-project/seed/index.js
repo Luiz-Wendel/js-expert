0f91ee89458b9957be6a5f28c48da7d5ecf1e07c
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-09-29 10:14:58 -0300

    add Customer seed

--- 01-js-testing/05-tdd-project/seed/index.js
+++ 01-js-testing/05-tdd-project/seed/index.js
@@ -8,6 +8,7 @@ const faker = require('faker');
 // local
 const Car = require('./../src/entities/car');
 const CarCategory = require('./../src/entities/carCategory');
+const Customer = require('./../src/entities/customer');
 
 const databaseBaseFolder = join(__dirname, '../', 'database');
 const ITEMS_AMOUT = 2;
@@ -20,6 +21,7 @@ const carCategory = new CarCategory({
 });
 
 const cars = [];
+const customers = [];
 for (let index = 0; index <= ITEMS_AMOUT; index += 1) {
   const car = new Car({
     id: faker.random.uuid(),
@@ -31,6 +33,14 @@ for (let index = 0; index <= ITEMS_AMOUT; index += 1) {
 
   carCategory.carIds.push(car.id);
   cars.push(car);
+
+  const customer = new Customer({
+    id: faker.random.uuid(),
+    name: faker.name.findName(),
+    age: faker.random.number({ min: 18, max: 50 }),
+  });
+
+  customers.push(customer);
 }
 
 const write = (filename, data) => writeFile(
@@ -41,4 +51,5 @@ const write = (filename, data) => writeFile(
 (async () => {
   await write('carCategories.json', [carCategory]);
   await write('cars.json', cars);
+  await write('customers.json', customers);
 })();

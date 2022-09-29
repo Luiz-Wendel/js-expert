8d03a23d2628f59f6e1ba98aa71359fa14754885
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-09-29 10:09:54 -0300

    add CarCategory seed

--- 01-js-testing/05-tdd-project/seed/index.js
+++ 01-js-testing/05-tdd-project/seed/index.js
@@ -1,1 +1,27 @@
-console.log('Seeding');
+// native
+const { join } = require('path');
+const { writeFile } = require('fs/promises');
+
+// third party
+const faker = require('faker');
+
+// local
+const CarCategory = require('./../src/entities/carCategory');
+
+const databaseBaseFolder = join(__dirname, '../', 'database');
+
+const carCategory = new CarCategory({
+  id: faker.random.uuid(),
+  name: faker.vehicle.type(),
+  carIds: [],
+  price: faker.finance.amount(20, 100),
+});
+
+const write = (filename, data) => writeFile(
+  join(databaseBaseFolder, filename),
+  JSON.stringify(data),
+);
+
+(async () => {
+  await write('carCategories.json', [carCategory]);
+})();

00243a29efa2f8df96661dfb51e920533fc16e49
Luiz Wendel <luiz.balbino@mercadolivre.com>
2023-06-25 14:09:27 -0300

    feat: add error messages

---
+++ src/01-testing/01-mocks/src/constants.js
@@ -0,0 +1,10 @@
+const constants = {
+  errors: {
+    messages: {
+      FILE_LENGTH_ERROR: 'The content length is invalid',
+      FILE_FIELDS_ERROR: 'The provided properties are invalid',
+    },
+  },
+};
+
+module.exports = constants;

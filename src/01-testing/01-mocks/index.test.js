9a8568ca8ebdd9fda99e12f14f22c0e021ac5730
Luiz Wendel <luiz.balbino@mercadolivre.com>
2023-06-25 14:53:58 -0300

    test: add valid test case - threeItems

--- src/01-testing/01-mocks/index.test.js
+++ src/01-testing/01-mocks/index.test.js
@@ -4,7 +4,7 @@ const { errors } = require('./src/constants');
 const File = require('./src/file');
 
 (async () => {
-  const rejectTestCases = [
+  const invalidTestCases = [
     {
       filePath: './mocks/invalid-emptyContent.csv',
       expected: new Error(errors.messages.FILE_LENGTH_ERROR),
@@ -19,9 +19,35 @@ const File = require('./src/file');
     },
   ];
 
-  rejectTestCases.forEach(async (testCase) => {
+  invalidTestCases.forEach(async (testCase) => {
     const result = File.csvToJson(testCase.filePath);
 
     await assert.rejects(result, testCase.expected);
+  });
+
+  const validTestCases = [
+    {
+      filePath: './mocks/valid-threeItems.csv',
+      expected: [
+        {
+          id: 1,
+          name: 'Jane Doe',
+          profession: 'developer',
+          age: 40,
+        },
+        {
+          id: 2,
+          name: 'Jhon Doe',
+          profession: 'manager',
+          age: 40,
+        },
+      ],
+    },
+  ];
+
+  validTestCases.forEach(async (testCase) => {
+    const result = await File.csvToJson(testCase.filePath);
+
+    assert.deepEqual(result, testCase.expected);
   });
 })();

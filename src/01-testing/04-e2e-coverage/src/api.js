b718051fe609cd79c67cb9285387fbb4d3cb4962
Luiz Wendel <luiz.balbino@mercadolivre.com>
2023-06-30 18:25:44 -0300

    feat: add routes system

--- src/01-testing/04-e2e-coverage/src/api.js
+++ src/01-testing/04-e2e-coverage/src/api.js
@@ -1,7 +1,26 @@
 const http = require('http');
 
+const routes = {
+  'get:/contact': (_req, res) => {
+    res.write('contact us page');
+
+    return res.end();
+  },
+  default(_req, res) {
+    res.writeHead(404);
+
+    return res.end('page not found');
+  },
+};
+
 function handler(req, res) {
-  return res.end('ok');
+  const { url, method } = req;
+
+  const routeKey = `${method.toLowerCase()}:${url}`;
+
+  const chosen = routes[routeKey] || routes.default;
+
+  return chosen(req, res);
 }
 
 http.createServer(handler)

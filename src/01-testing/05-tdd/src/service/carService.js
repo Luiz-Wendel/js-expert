507756660a0b8360c986c21e441db81378da9157
Luiz Wendel <luiz.balbino@mercadolivre.com>
2023-07-01 12:06:40 -0300

    feat: implements car service getFinalPrice method

--- src/01-testing/05-tdd/src/service/carService.js
+++ src/01-testing/05-tdd/src/service/carService.js
@@ -1,10 +1,20 @@
 /* eslint-disable class-methods-use-this */
 
+// repositories
 const BaseRepository = require('../repository/base/baseRepository');
+
+// entities
+const Tax = require('../entities/tax');
 
 class CarService {
   constructor({ cars }) {
     this.carRepository = new BaseRepository({ file: cars });
+
+    this.taxesBasedOnAge = Tax.taxesBasedOnAge;
+    this.currencyFormat = new Intl.NumberFormat('pt-br', {
+      style: 'currency',
+      currency: 'BRL',
+    });
   }
 
   getRandomPositionFromArray(list) {
@@ -25,6 +35,19 @@ class CarService {
     const car = await this.carRepository.find(carId);
 
     return car;
+  }
+
+  getFinalPrice(customer, carCategory, numberOfDays) {
+    const { age } = customer;
+    const { price } = carCategory;
+    const getTaxBasedOnAge = (tax) => age >= tax.from && age <= tax.to;
+    const { then: tax } = this.taxesBasedOnAge
+      .find(getTaxBasedOnAge);
+
+    const finalPrice = (tax * price) * numberOfDays;
+    const formattedPrice = this.currencyFormat.format(finalPrice);
+
+    return formattedPrice;
   }
 }
 

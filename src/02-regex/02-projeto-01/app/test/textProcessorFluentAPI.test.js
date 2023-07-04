0cd6cfa3389823c1149c9dc3a464642e1304fbf1
Luiz Wendel <luiz.balbino@mercadolivre.com>
2023-07-04 00:02:54 -0300

    test: add extractPeopleData text processor test

--- src/02-regex/02-projeto-01/app/test/textProcessorFluentAPI.test.js
+++ src/02-regex/02-projeto-01/app/test/textProcessorFluentAPI.test.js
@@ -12,4 +12,27 @@ describe('TextProcessorFluentAPI', () => {
 
     expect(result).to.be.equal(mock);
   });
+
+  it('#extractPeopleData', () => {
+    const result = new TextProcessorFluentAPI(mock)
+      .extractPeopleData()
+      .build();
+
+    const expected = [
+      [
+        'Xuxa da Silva, brasileira, casada, CPF 235.743.420-12, residente e ',
+        'domiciliada a Rua dos bobos, zero, bairro Alphaville, São Paulo. ',
+      ].join('\n'),
+      [
+        'Arya Robbin, belga, casado, CPF 884.112.200-52, residente e ',
+        'domiciliada a Av. paulista, 1400, bairro Consolação, São Paulo. ',
+      ].join('\n'),
+      [
+        'Júlia Menezes, brasileira, solteira, CPF 297.947.800-81, residente e ',
+        'domiciliada a Av. dos Estados, 99, bairro Jardins, São Paulo. ',
+      ].join('\n'),
+    ];
+
+    expect(result).to.be.deep.equal(expected);
+  });
 });

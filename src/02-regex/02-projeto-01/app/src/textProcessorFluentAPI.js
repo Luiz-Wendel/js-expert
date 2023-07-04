3447d604aefc291d6033422da7b65afb79af6d39
Luiz Wendel <luiz.balbino@mercadolivre.com>
2023-07-04 00:03:47 -0300

    feat: implements text processor extractPeopleData method

--- src/02-regex/02-projeto-01/app/src/textProcessorFluentAPI.js
+++ src/02-regex/02-projeto-01/app/src/textProcessorFluentAPI.js
@@ -6,6 +6,30 @@ class TextProcessorFluentAPI {
     this.#content = content;
   }
 
+  extractPeopleData() {
+    // ?<= -> lookbehind (extract everything that is after the pattern)
+    // [contratante|contratada] -> find the words 'contratante' or 'contratada'
+    // :\s{1} -> find the character ':' followed by a space
+    // All of above in a group to get the following content (lookbehind)
+
+    // (?!\\s) -> negative lookahead (ignore following space)
+
+    // .*\n -> anything followed by a line break
+    // .*? -> anything (lazy) (? non greety, stop on the first match)
+    // $ -> end of line
+
+    // gmi -> global, multiline, case insensitive
+
+    const matchPerson = /(?<=[contratante|contratada]:\s{1})(?!\s)(.*\n.*?)$/gmi;
+
+    // match -> return an array with the matches
+    const onlyPerson = this.#content.match(matchPerson);
+
+    this.#content = onlyPerson;
+
+    return this;
+  }
+
   build() {
     return this.#content;
   }

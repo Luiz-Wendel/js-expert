5fbaf6688d5035420df08b365e115845d12db8ae
Luiz Wendel <luiz.balbino@mercadolivre.com>
2023-07-15 17:00:25 -0300

    feat: implements TableBrowserComponent prepareData method

--- src/03-design-patterns/creational/02-abstract-factory/platforms/browser/table.mjs
+++ src/03-design-patterns/creational/02-abstract-factory/platforms/browser/table.mjs
@@ -1,9 +1,39 @@
 /* eslint-disable import/extensions */
+/* eslint-disable class-methods-use-this */
 
 import TableComponent from '../../shared/base/tableComponent.mjs';
 
 export default class TableBrowserComponent extends TableComponent {
   render(data) {
-    window.alert('called!');
+    const template = this.prepareData(data);
+    console.log(template);
+  }
+
+  prepareData(data) {
+    const [firtsItem] = data;
+    const tHeaders = Object.keys(firtsItem)
+      .map((text) => `<th scope=col>${text}</th>`);
+
+    const joinLists = (list) => list.join('');
+
+    const tBodyValues = data
+      .map((item) => Object.values(item)
+        .map((value) => `<td>${value}</td>`))
+      .map((tds) => `<tr>${joinLists(tds)}</tr>`);
+
+    const template = `
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            ${joinLists(tHeaders)}
+          </tr>
+        </thead>
+        <tbody>
+          ${joinLists(tBodyValues)}
+        </tbody>
+      </table>
+    `;
+
+    return template;
   }
 }

8e22610fc73a56237ae34964450094679913a321
Luiz Wendel <luiz.balbino@mercadolivre.com>
2023-07-15 17:13:56 -0300

    feat: implements TableConsoleComponent prepareData method

--- src/03-design-patterns/creational/02-abstract-factory/platforms/console/table.mjs
+++ src/03-design-patterns/creational/02-abstract-factory/platforms/console/table.mjs
@@ -1,9 +1,32 @@
 /* eslint-disable import/extensions */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import chalk from 'chalk';
 
 import TableComponent from '../../shared/base/tableComponent.mjs';
 
 export default class TableConsoleComponent extends TableComponent {
   render(data) {
-    console.log('called!', data);
+    const columns = this.prepareData(data);
+    console.log(columns);
+  }
+
+  prepareData(data) {
+    const [firtsItem] = data;
+    const headers = Object.keys(firtsItem);
+
+    const formatHeader = (item, index) => (
+      index % 2 === 0
+        ? chalk.yellow(item)
+        : chalk.green(item)
+    );
+
+    const columns = headers.map((item, index) => ({
+      field: item,
+      name: formatHeader(item, index),
+    }));
+
+    return columns;
   }
 }
